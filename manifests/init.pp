4a53277f53d914f86017db925517ca6d177a9288
Johan Huysmans <johan.huysmans@inuits.be>
2012-01-06 10:25:49 +0100

    Added several checks

--- manifests/init.pp
+++ manifests/init.pp
@@ -56,6 +56,7 @@ class nrpe::check_ping {
       group   => "root",
       mode    => 0644,
       content => "command[check_ping]=/usr/lib/nagios/plugins/check_ping -4 -H \$ARG1$ -w 2000,50% -c 5000,75%\ncommand[check_ping6]=/usr/lib/nagios/plugins/check_ping -6 -H \$ARG1$ -w 2000,50% -c 5000,75%\n",
+      require => Package["nrpe"],
       notify  => Service["nrpe"];
   }
 
@@ -87,6 +88,141 @@ define nrpe::check_ping6::storeconfig {
       host_name           => "${hostname}",
       use                 => 'ntc-service',
       tag                 => 'icinga';
+  }
+
+}
+
+class nrpe::check_file_age {
+
+  package {
+    "nagios-plugins-file_age":
+      ensure => installed;
+  }
+
+  file {
+    "/etc/nrpe.d/command_check_file_age.cfg":
+      owner   => "root",
+      group   => "root",
+      mode    => 0644,
+      content => "command[check_file_age]=/usr/lib/nagios/plugins/check_file_age -f \$ARG1$ -w \$ARG2$ -c \$ARG3$ -W \$ARG4$ -C \$ARG5$ \n",
+      require => Package["nrpe"],
+      notify  => Service["nrpe"];
+  }
+
+}
+
+define nrpe::check_file_age::storeconfig ($warn_secs = 240,
+                                          $crit_secs = 600,
+                                          $warn_size = 0,
+                                          $crit_size = 0) {
+
+  include nrpe::check_file_age
+
+  @@nagios_service {
+    "check_file_age_${hostname}_${name}":
+      check_command       => "check_file_age!${name}!${warn_secs}!${crit_secs}!${warn_size}!${crit_size}",
+      service_description => inline_template("FILE AGE <%= File.basename(name) %>"),
+      host_name           => "${hostname}",
+      use                 => 'ntc-service',
+      tag                 => 'icinga';
+  }
+
+}
+
+class nrpe::nagios-plugins-load {
+
+  package {
+    "nagios-plugins-load":
+      ensure => installed;
+  }
+
+  file {
+    "/etc/nrpe.d/command_nagios-plugins-load.cfg":
+      owner   => "root",
+      group   => "root",
+      mode    => 0644,
+      content => "command[check_load]=/usr/lib/nagios/plugins/check_load -w \$ARG1$ -c \$ARG2$ \n",
+      require => Package["nrpe"],
+      notify  => Service["nrpe"];
+  }
+
+}
+
+class nrpe::nagios-plugins-users {
+
+  package {
+    "nagios-plugins-users":
+      ensure => installed;
+  }
+
+  file {
+    "/etc/nrpe.d/command_nagios-plugins-users.cfg":
+      owner   => "root",
+      group   => "root",
+      mode    => 0644,
+      content => "command[check_users]=/usr/lib/nagios/plugins/check_users -w \$ARG1$ -c \$ARG2$ \n",
+      require => Package["nrpe"],
+      notify  => Service["nrpe"];
+  }
+
+}
+
+class nrpe::nagios-plugins-procs {
+
+  package {
+    "nagios-plugins-procs":
+      ensure => installed;
+  }
+
+  file {
+    "/etc/nrpe.d/command_nagios-plugins-procs.cfg":
+      owner   => "root",
+      group   => "root",
+      mode    => 0644,
+      content => "command[check_procs]=/usr/lib/nagios/plugins/check_procs -w \$ARG1$ -c \$ARG2$ \n",
+      require => Package["nrpe"],
+      notify  => Service["nrpe"];
+  }
+
+}
+
+class nrpe::nagios-plugins-hpasm {
+
+  package {
+    "nagios-plugins-hpasm":
+      ensure => installed;
+  }
+
+  file {
+    "/etc/nrpe.d/command_nagios-plugins-hpasm.cfg":
+      owner   => "root",
+      group   => "root",
+      mode    => 0644,
+      content => "command[check_hpasm]=/usr/lib/nagios/plugins/check_hpasm \n",
+      require => Package["nrpe"],
+      notify  => Service["nrpe"];
+  }
+
+}
+
+class nrpe::nagios-plugins-ntc {
+
+  package {
+    "nagios-plugins-ntc":
+      ensure => installed;
+  }
+
+  file {
+    "/etc/nrpe.d/command_nagios-plugins-ntc.cfg":
+      owner   => "root",
+      group   => "root",
+      mode    => 0644,
+      content => "\
+command[check_cpu_usage]=/usr/lib/nagios/plugins/check_cpu_usage
+command[check_mem_usage]=/usr/lib/nagios/plugins/check_mem_usage
+",
+      require => Package["nrpe"],
+      notify  => Service["nrpe"];
   }
 
 }
