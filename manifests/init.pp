798bb60b89394efa1ae3861d60f1af604f27d736
Johan Huysmans <johan@desk-jhu.localdomain>
2012-10-11 10:43:59 +0200

    Add plugins + fix puppet-lint errors

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,63 +1,63 @@
 class nrpe ($allowed_hosts) {
 
   package {
-    "nrpe":
+    'nrpe':
       ensure => installed;
   }
 
   service {
-    "nrpe":
-      enable  => true,
+    'nrpe':
       ensure  => running,
-      require => Package["nrpe"];
+      enable  => true,
+      require => Package['nrpe'];
   }
 
   file {
-    "/etc/nrpe.d/config.cfg":
-      owner   => "root",
-      group   => "root",
-      mode    => 0644,
-      content => template("nrpe/config.cfg.erb"),
-      require => Package["nrpe"],
-      notify  => Service["nrpe"];
-    "/etc/nagios/nrpe.cfg":
-      owner   => "root",
-      group   => "root",
-      mode    => "0644",
+    '/etc/nrpe.d/config.cfg':
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      content => template('nrpe/config.cfg.erb'),
+      require => Package['nrpe'],
+      notify  => Service['nrpe'];
+    '/etc/nagios/nrpe.cfg':
       ensure  => file,
-      require => Package["nrpe"];
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      require => Package['nrpe'];
   }
 
 }
 
 class nrpe::storeconfig {
 
-  @@nagios_service { 
+  @@nagios_service {
     "check_nrpe_${hostname}":
       check_command       => 'check_nrpe',
       service_description => 'NRPE',
-      host_name           => "${hostname}",
+      host_name           => ${hostname},
       use                 => 'ntc-service',
       tag                 => 'icinga';
   }
-    
+
 }
 
 class nrpe::check_ping {
 
   package {
-    "nagios-plugins-ping":
+    'nagios-plugins-ping':
       ensure => installed;
   }
 
   file {
-    "/etc/nrpe.d/command_check_ping.cfg":
-      owner   => "root",
-      group   => "root",
-      mode    => 0644,
-      content => "command[check_ping]=/usr/lib/nagios/plugins/check_ping -4 -H \$ARG1$ -w 2000,50% -c 5000,75%\ncommand[check_ping6]=/usr/lib/nagios/plugins/check_ping -6 -H \$ARG1$ -w 2000,50% -c 5000,75%\n",
-      require => Package["nrpe"],
-      notify  => Service["nrpe"];
+    '/etc/nrpe.d/command_check_ping.cfg':
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      content => 'command[check_ping]=/usr/lib/nagios/plugins/check_ping -4 -H \$ARG1$ -w 2000,50% -c 5000,75%\ncommand[check_ping6]=/usr/lib/nagios/plugins/check_ping -6 -H \$ARG1$ -w 2000,50% -c 5000,75%\n',
+      require => Package['nrpe'],
+      notify  => Service['nrpe'];
   }
 
 }
@@ -70,7 +70,7 @@ define nrpe::check_ping::storeconfig {
     "check_ping_${hostname}_${name}":
       check_command       => "check_ping_remote!${name}",
       service_description => "PING ${name}",
-      host_name           => "${hostname}",
+      host_name           => ${hostname},
       use                 => 'ntc-service',
       tag                 => 'icinga';
   }
@@ -85,7 +85,7 @@ define nrpe::check_ping6::storeconfig {
     "check_ping6_${hostname}_${name}":
       check_command       => "check_ping6_remote!${name}",
       service_description => "PING6 ${name}",
-      host_name           => "${hostname}",
+      host_name           => ${hostname},
       use                 => 'ntc-service',
       tag                 => 'icinga';
   }
@@ -95,18 +95,18 @@ define nrpe::check_ping6::storeconfig {
 class nrpe::check_file_age {
 
   package {
-    "nagios-plugins-file_age":
+    'nagios-plugins-file_age':
       ensure => installed;
   }
 
   file {
-    "/etc/nrpe.d/command_check_file_age.cfg":
-      owner   => "root",
-      group   => "root",
-      mode    => 0644,
-      content => "command[check_file_age]=/usr/lib/nagios/plugins/check_file_age -f \$ARG1$ -w \$ARG2$ -c \$ARG3$ -W \$ARG4$ -C \$ARG5$ \n",
-      require => Package["nrpe"],
-      notify  => Service["nrpe"];
+    '/etc/nrpe.d/command_check_file_age.cfg':
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      content => 'command[check_file_age]=/usr/lib/nagios/plugins/check_file_age -f \$ARG1$ -w \$ARG2$ -c \$ARG3$ -W \$ARG4$ -C \$ARG5$ \n',
+      require => Package['nrpe'],
+      notify  => Service['nrpe'];
   }
 
 }
@@ -122,7 +122,7 @@ define nrpe::check_file_age::storeconfig ($warn_secs = 240,
     "check_file_age_${hostname}_${name}":
       check_command       => "check_file_age!${name}!${warn_secs}!${crit_secs}!${warn_size}!${crit_size}",
       service_description => inline_template("FILE AGE <%= File.basename(name) %>"),
-      host_name           => "${hostname}",
+      host_name           => ${hostname},
       use                 => 'ntc-service',
       tag                 => 'icinga';
   }
@@ -132,18 +132,18 @@ define nrpe::check_file_age::storeconfig ($warn_secs = 240,
 class nrpe::nagios-plugins-load {
 
   package {
-    "nagios-plugins-load":
+    'nagios-plugins-load':
       ensure => installed;
   }
 
   file {
-    "/etc/nrpe.d/command_nagios-plugins-load.cfg":
-      owner   => "root",
-      group   => "root",
-      mode    => 0644,
-      content => "command[check_load]=/usr/lib/nagios/plugins/check_load -w \$ARG1$ -c \$ARG2$ \n",
-      require => Package["nrpe"],
-      notify  => Service["nrpe"];
+    '/etc/nrpe.d/command_nagios-plugins-load.cfg':
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      content => 'command[check_load]=/usr/lib/nagios/plugins/check_load -w \$ARG1$ -c \$ARG2$ \n',
+      require => Package['nrpe'],
+      notify  => Service['nrpe'];
   }
 
 }
@@ -151,18 +151,18 @@ class nrpe::nagios-plugins-load {
 class nrpe::nagios-plugins-users {
 
   package {
-    "nagios-plugins-users":
+    'nagios-plugins-users':
       ensure => installed;
   }
 
   file {
-    "/etc/nrpe.d/command_nagios-plugins-users.cfg":
-      owner   => "root",
-      group   => "root",
-      mode    => 0644,
-      content => "command[check_users]=/usr/lib/nagios/plugins/check_users -w \$ARG1$ -c \$ARG2$ \n",
-      require => Package["nrpe"],
-      notify  => Service["nrpe"];
+    '/etc/nrpe.d/command_nagios-plugins-users.cfg':
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      content => 'command[check_users]=/usr/lib/nagios/plugins/check_users -w \$ARG1$ -c \$ARG2$ \n',
+      require => Package['nrpe'],
+      notify  => Service['nrpe'];
   }
 
 }
@@ -170,18 +170,18 @@ class nrpe::nagios-plugins-users {
 class nrpe::nagios-plugins-procs {
 
   package {
-    "nagios-plugins-procs":
+    'nagios-plugins-procs':
       ensure => installed;
   }
 
   file {
-    "/etc/nrpe.d/command_nagios-plugins-procs.cfg":
-      owner   => "root",
-      group   => "root",
-      mode    => 0644,
-      content => "command[check_procs]=/usr/lib/nagios/plugins/check_procs -w \$ARG1$ -c \$ARG2$ \n",
-      require => Package["nrpe"],
-      notify  => Service["nrpe"];
+    '/etc/nrpe.d/command_nagios-plugins-procs.cfg':
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      content => 'command[check_procs]=/usr/lib/nagios/plugins/check_procs -w \$ARG1$ -c \$ARG2$ \n',
+      require => Package['nrpe'],
+      notify  => Service['nrpe'];
   }
 
 }
@@ -189,18 +189,25 @@ class nrpe::nagios-plugins-procs {
 class nrpe::nagios-plugins-hpasm {
 
   package {
-    "nagios-plugins-hpasm":
+    'nagios-plugins-hpasm':
       ensure => installed;
   }
 
   file {
-    "/etc/nrpe.d/command_nagios-plugins-hpasm.cfg":
-      owner   => "root",
-      group   => "root",
-      mode    => 0644,
-      content => "command[check_hpasm]=/usr/lib/nagios/plugins/check_hpasm \n",
-      require => Package["nrpe"],
-      notify  => Service["nrpe"];
+    '/etc/nrpe.d/command_nagios-plugins-hpasm.cfg':
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      content => 'command[check_hpasm]=/usr/lib/nagios/plugins/check_hpasm \n',
+      require => Package['nrpe'],
+      notify  => Service['nrpe'];
+  }
+
+  sudo::spec {
+    'nrpe_hpasmcli':
+      users    => 'nrpe',
+      hosts    => 'ALL',
+      commands => '(ALL) NOPASSWD: /sbin/hpasmcli';
   }
 
 }
@@ -208,21 +215,68 @@ class nrpe::nagios-plugins-hpasm {
 class nrpe::nagios-plugins-ntc {
 
   package {
-    "nagios-plugins-ntc":
+    'nagios-plugins-ntc':
       ensure => installed;
   }
 
   file {
-    "/etc/nrpe.d/command_nagios-plugins-ntc.cfg":
-      owner   => "root",
-      group   => "root",
-      mode    => 0644,
-      content => "\
+    '/etc/nrpe.d/command_nagios-plugins-ntc.cfg':
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      content => '\
 command[check_cpu_usage]=/usr/lib/nagios/plugins/check_cpu_usage
 command[check_mem_usage]=/usr/lib/nagios/plugins/check_mem_usage
-",
-      require => Package["nrpe"],
-      notify  => Service["nrpe"];
+command[check_java_memory]=/usr/lib/nagios/plugins/check_java_memory -p \$ARG1$
+command[check_whoami]=/usr/lib/nagios/plugins/check_whoami
+',
+      require => Package['nrpe'],
+      notify  => Service['nrpe'];
+  }
+
+  sudo::spec {
+    'nrpe_jstat':
+      users    => 'nrpe',
+      hosts    => 'ALL',
+      commands => '(ALL) NOPASSWD: /usr/bin/jstat';
+  }
+
+}
+
+class nrpe::nagios-plugins-swap {
+
+  package {
+    'nagios-plugins-swap':
+      ensure => installed;
+  }
+
+  file {
+    '/etc/nrpe.d/command_nagios-plugins-swap.cfg':
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      content => 'command[check_swap]=/usr/lib/nagios/plugins/check_swap -w \$ARG1$ -c \$ARG2$ \n',
+      require => Package['nrpe'],
+      notify  => Service['nrpe'];
+  }
+
+}
+
+class nrpe::nagios-plugins-tellitec {
+
+  package {
+    'nagios-plugins-nrpe-tellitec':
+      ensure => installed;
+  }
+
+  file {
+    '/etc/nrpe.d/command_nagios-plugins-tellitec.cfg':
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      content => 'command[check_tc-shape-server_shaping-ratio]=/usr/lib/nagios/plugins/check_tc-shape-server_accounting --instances \$ARG1$ --group \$ARG2$ --statistic \'Minimum Shaping Ratio\' --statistic \'Maximum Shaping Ratio\' --statistic \'Average Shaping Ratio\' \n',
+      require => Package['nrpe'],
+      notify  => Service['nrpe'];
   }
 
 }
